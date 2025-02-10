773b67789748d1013f3796bb0d4a414c6914e7a8
davidhuynhgit <htdatbk@gmail.com>
2025-02-10 20:42:55 +1000

    Update indicator

--- src/pages/about.js
+++ src/pages/about.js
@@ -88,7 +88,7 @@ const about = () => {
             <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumber value={50} />+
+                  <AnimatedNumber value={20} />+
                 </span>
                 <h2 className="text-x1 font-medium capitalize text-dark/75 dark:text-light
                 xl:text-center md:text-lg sm:text-base xs:text-sm">
@@ -98,7 +98,7 @@ const about = () => {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumber value={40} />+
+                  <AnimatedNumber value={15} />+
                 </span>
                 <h2 className="text-x1 font-medium capitalize text-dark/75 dark:text-light
                 xl:text-center md:text-lg sm:text-base xs:text-sm">
