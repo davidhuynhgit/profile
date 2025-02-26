15b315fad7b72eb80771131cefe2bb8cbcafac41
davidhuynhgit <htdatbk@gmail.com>
2025-02-26 14:27:31 +1000

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,15 +40,15 @@ export default function Home() {
 
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/Resume.pdf" target={"_blank"}
-                  className ="flex items-center bg-dark text-light p-2.5 px-6
-                  rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
-                  border-2 border-solid border-transparent hover:border-dark
+                  className ="flex items-center p-2 px-4 !w-[142px] !h-[50px]
+                  bg-dark text-light 
                   dark:bg-light dark:text-dark 
-                  hover:dark:bg-dark hover:dark:text-light
-                  hover:dark:border-light
-                  md:p-2 md:px-4 md:text-base"
+                  rounded-lg text-lg font-semibold 
+                  border-2 border-solid border-transparent hover:border-dark hover:dark:border-light
+                  hover:bg-light hover:text-dark
+                  hover:dark:bg-dark hover:dark:text-light              "
                   download={true}
-                >Resume <LinkArrow className={"w-6 ml-1"}/>
+                >Resume <LinkArrow  className="ml-2 !h-[50px] !w-[30px]"  />
                 </Link>
                 <Link href="mailto:davidhuynh.connect@gmail.com"
                 className='ml-4 text-lg font-medium capitalize 
