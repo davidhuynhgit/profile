99b6636814fcbe3944ee09d762ba6f08f33bd84b
davidhuynhgit <htdatbk@gmail.com>
2025-02-26 13:41:29 +1000

    Update QR

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/component/Layout'
 import profilePic from "../../public/images/profile/IndexPhoto.png"
+import QRPic from "../../public/images/profile/QR.png"
 import AnimatedText from '@/component/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/component/Icon'
@@ -30,7 +31,7 @@ export default function Home() {
             </div>
 
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-              <AnimatedText text="Unleashing The Power Of Data To Drive Smart Decisions." className='!text-6xl !text-left
+              <AnimatedText text="Unleash The Power Of Data To Drive Smart Decisions." className='!text-6xl !text-left
               xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'/>            
               <p className='my-4 text-base font-medium'>
               As a passionate data analyst/engineer with an automation engineering background, I specialize in extracting meaningful insights through data mining, machine learning, and advanced analytics. 
@@ -38,24 +39,41 @@ export default function Home() {
               </p>
 
               <div className='flex items-center self-start mt-2 lg:self-center'>
-              <Link href="/Resume.pdf" target={"_blank"}
-                className ="flex items-center bg-dark text-light p-2.5 px-6
-                rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
-                border-2 border-solid border-transparent hover:border-dark
-                dark:bg-light dark:text-dark 
-                hover:dark:bg-dark hover:dark:text-light
-                hover:dark:border-light
-                md:p-2 md:px-4 md:text-base"
-                download={true}
-              >Resume <LinkArrow className={"w-6 ml-1"}/>
-              </Link>
-              <Link href="mailto:davidhuynh.connect@gmail.com"
-              className='ml-4 text-lg font-medium capitalize 
-              text-dark underline dark:text-light
-              md:text-base
-              '
-              >Contact</Link>
+                <Link href="/Resume.pdf" target={"_blank"}
+                  className ="flex items-center bg-dark text-light p-2.5 px-6
+                  rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                  border-2 border-solid border-transparent hover:border-dark
+                  dark:bg-light dark:text-dark 
+                  hover:dark:bg-dark hover:dark:text-light
+                  hover:dark:border-light
+                  md:p-2 md:px-4 md:text-base"
+                  download={true}
+                >Resume <LinkArrow className={"w-6 ml-1"}/>
+                </Link>
+                <Link href="mailto:davidhuynh.connect@gmail.com"
+                className='ml-4 text-lg font-medium capitalize 
+                text-dark underline dark:text-light
+                md:text-base'
+                >Contact</Link>              
               </div>
+
+              <div className='w-full justify-items-end 
+              lg:justify-items-center lg:mt-6 '>
+                <div className='flex flex-col items-center'>
+                  <Image 
+                    src={QRPic} 
+                    alt="David" 
+                    className='border border-dark'
+                    priority
+                    width={180} 
+                    height={180} 
+                  />
+                  <p className='p-2 text-dark dark:text-light font-bold text-lg'>
+                    Scan & Share
+                  </p>
+                </div>
+              </div>
+
             </div>
           </div>
         </Layout>
