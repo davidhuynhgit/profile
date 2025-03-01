58a427a3ec331c819cdb4ac9504f769faf5ffaf0
davidhuynhgit <htdatbk@gmail.com>
2025-03-01 22:02:17 +1000

    Update milestones

--- src/component/Milestone.js
+++ src/component/Milestone.js
@@ -59,48 +59,104 @@ const Details = ({ logo, title, time, place, info }) => {
                             md:left-[calc(10%+25px)] xs:left-[25px]
                             left-[calc(20%+25px)]"
                         />
+                        
+                        <Details
+                            logo={iQUT}
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience."                   
+                        />
+
+                        <Details
+                            logo={iTED}
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience.
+                            <a href='https://www.tedxqut.com/events' target='_blank'>
+                                Check this out.
+                            </a>"                   
+                        />
+
+                        <Details
+                            logo={iConnect}
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience."                   
+                        />
+
+
+                        <Details
+                            logo={iBANDS}
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience."                   
+                        />
 
                         <Details
                             logo={iQUT}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience."                   
                         />
+
                         <Details
-                            logo={iBANDS}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
+                            logo={iHamon}
+                            title="Involvement in a Complex National Energy Project"
+                            time="2021"
+                            place="Hamon Vietnam"
+                            info="Gained significant experience in Instrument & Control (I&C) through direct collaboration with seasoned professionals. 
+                            This exceptional opportunity allowed me to expand my knowledge base and learn from international colleagues with extensive field experience."                   
                         />
+
+                        <Details
+                            logo={iSistech}
+                            title="Entry into the Professional Field"
+                            time="2020"
+                            place="SISTECH Co., Ltd"
+                            info="Successfully transitioned from academic studies to my first professional role at SISTECH. 
+                            This position allowed me to apply my skills in automation engineering through practical projects.
+                            I also developed teaching methodologies in our training courses for partners and customers."                   
+                        />
+
+                        <Details
+                            logo={iSpringer}
+                            title="International research publication"
+                            time="2019"
+                            place=""
+                            info="Achieved publication through a collaborative research effort, showcasing my ability to contribute to original work. 
+                            The process involved a deliberate exploration of varied methodologies, ultimately leading to a successful outcome.                            
+                            <a href='https://link.springer.com/chapter/10.1007/978-3-030-53021-1_53' target='_blank'>
+                                Check this out.
+                            </a>"                       
+                        />
+
                         <Details
                             logo={iBK}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
+                            title="Electrical and electronics engineering degrees"
+                            time="2014-2021"
+                            place="Ho Chi Minh City University of Technology (HCMUT)"
+                            info="Completed Bachelor's and Master's degrees in Control and Automation, establishing a strong technical foundation.
+                            They provided me with a rigorous academic experience, cultivating analytical skills and a disciplined approach.
+                            These qualities remain central to my professional and personal growth."
                         />
+                        
                         <Details
-                            logo={iQUT}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
-                        />
-                        <Details
-                            logo={iBANDS}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
-                        />
-                        <Details
-                            logo={iBK}
-                            title="Online Coursework"
-                            time="2016-2020"
-                            place="Coursera And EdX"
-                            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine Learning Engineering."
+                            logo={iLHP}
+                            title="High School (for the Gifted)"
+                            time="2011-2014"
+                            place="Lê Hồng Phong High School for the Gifted"
+                            info="My time at a leading high school presented significant challenges, demanding resilience and determination.
+                            I learned to advocate for myself, fostering a strong sense of independence."
                         />
                     </ul>
                 </div>
